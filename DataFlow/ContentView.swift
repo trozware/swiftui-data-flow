ba943666e52f79a6bb0c4b6aa11c6054e21db562
Sarah Reichelt <sarah@troz.net>
2019-09-14 13:42:11 +1000

    Comments & formatting

--- DataFlow/ContentView.swift
+++ DataFlow/ContentView.swift
@@ -9,19 +9,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    let greeting = "Hello SwiftUI!"
+    // Property
+    let greeting = "Hello from SwiftUI!"
+
+    // State
     @State private var toggleValue = true
     @State private var stepperValue = 0
 
     var body: some View {
         VStack {
+            // Using property directly
             Text(greeting)
+                .font(.title)
 
+            // Using state with 2-way binding
             Toggle(isOn: $toggleValue) {
                 Text("Toggle is \(self.toggleValue ? "ON" : "OFF")")
             }
             .padding()
 
+            // Using state with 2-way binding
             NumberChooser(stepperValue: $stepperValue)
         }
     }
@@ -34,31 +41,39 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct NumberChooser: View {
+    // Using state from parent with 2-way binding
     @Binding var stepperValue: Int
 
     var body: some View {
-        VStack {
-            Stepper(value: $stepperValue, in: 0...20) {
-                Text("\(stepperValue)")
-            }
-            .padding()
+        ZStack {
+            RoundedRectangle(cornerRadius: 15)
+                .fill(Color.init(red: 0.95, green: 0.95, blue: 0.95))
 
-            NumberBlock(stepperValue: stepperValue)
+            VStack {
+                // Using bound state from parent with 2-way binding
+                Stepper(value: $stepperValue, in: 0...20) {
+                    Text("Value = \(stepperValue)")
+                }
+                .padding()
+
+                // Using bound state from parent as property
+                // this view cannot change the value
+                NumberBlock(stepperValue: stepperValue)
+            }
         }
-        .frame(width: 250, height: 190)
-        .background(Color.init(red: 0.95, green: 0.95, blue: 0.95))
         .padding()
     }
 }
 
 struct NumberBlock: View {
+    // As this view never changes the value, there is no need to bind it
     var stepperValue: Int
 
     var body: some View {
         Image(systemName: "\(stepperValue).square")
             .font(.system(size: 100))
             .foregroundColor(.blue)
-            .padding()
+            .padding(.bottom, 20)
     }
 }
 
